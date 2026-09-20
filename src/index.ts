a25305bafe747586b8b37e0589922198f59dae77
Mas1985200 <mas1985dimond@gmail.com>
2026-09-20 18:08:54 +0330

    Create index.ts for module exports and types

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { default as App } from "./App";
+
+export {
+  APP_METADATA,
+  DEFAULT_LOCALE,
+  LOCALES,
+  LOCALE_BY_CODE,
+  CAPABILITIES,
+  CAPABILITY_BY_ID,
+} from "./config";
+
+export type {
+  CapabilityDefinition,
+  CapabilityId,
+  AppDirection,
+  AppMetadata,
+  LocaleConfig,
+  SupportedLocale,
+} from "./config";
