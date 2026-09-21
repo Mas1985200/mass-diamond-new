5ff2b4faa3b95c53aead115f51e0a34bec668296
Mas1985200 <mas1985dimond@gmail.com>
2026-09-21 16:08:01 +0330

    Replace welcome section with ChatSurface component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { DiamondLogo } from "./components/brand";
+import ChatSurface from "./components/chat/ChatSurface";
 import AppShell from "./components/layout/AppShell";
-import { APP_METADATA } from "./config";
 
 function App() {
   return (
     <AppShell>
-      <section
-        aria-labelledby="mass-diamond-title"
-        className="md-welcome"
-      >
-        <DiamondLogo
-          size={112}
-          glow
-          decorative
-        />
-
-        <h1
-          id="mass-diamond-title"
-          className="md-welcome__title"
-        >
-          {APP_METADATA.name}
-        </h1>
-
-        <p className="md-welcome__tagline">
-          {APP_METADATA.tagline}
-        </p>
-      </section>
+      <ChatSurface />
     </AppShell>
   );
 }
